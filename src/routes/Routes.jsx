c904ae9d6cdb006285e7dc3b95019d08164de344
towhid046 <towhidmorol46@gmail.com>
2024-04-06 05:49:06 +0600

    first commit (setup the project)

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from  "react-router-dom";
+
+import Home from "./../pages/Home/Home";
+import Root from "./../layout/Root";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <div>404 ERROR</div>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+export default routes;
