7f4f9c62bb0947cf975cae7572dc0f6e811847d2
towhid046 <towhidmorol46@gmail.com>
2024-04-07 22:00:30 +0600

    added register page

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import SectionTitle from './../SectionTitle/SectionTitle';
+import SectionTitle from "./../SectionTitle/SectionTitle";
 
 const LeftSideNav = () => {
   const [categories, setCategories] = useState([]);
@@ -14,7 +14,7 @@ const LeftSideNav = () => {
 
   return (
     <div>
-      <SectionTitle title='All Category'/>
+      <SectionTitle title="All Category" />
       <ul className="flex flex-col items-start justify-start">
         {categories.map((category) => (
           <button
@@ -25,6 +25,23 @@ const LeftSideNav = () => {
           </button>
         ))}
       </ul>
+      <div>
+        <div className=" bg-base-100 border">
+          <figure>
+            <img
+              src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
